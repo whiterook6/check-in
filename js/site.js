b7380f1bbf766e6de104423e5aafc2729c2132ce
whiterook6 <whiterook6@gmail.com>
2015-11-21 00:06:39 -0800

    Adding resources for realz yo

--- js/site.js
+++ js/site.js
@@ -1,48 +1,88 @@
 (function(){
 	angular
 		.module('Check-in', ['ngResource'])
+		
 		.factory('Projects', ['$resource', function($resource){
-			return $resource('/api/projects/:id', {
-				id: '@id'
-			}, {
-				list: { method: 'GET' },
-				create: { method: 'POST' },
-				find: { method: 'GET' },
-				update: { method: 'POST' },
-				delete: { method: 'DELETE' }
-			});
-		}])
-		.factory('Designs', ['$resource', function($resource){
-			return $resource('/api/designs/:id', {
-				id: '@id'
-			}, {
-				find: { method: 'GET' },
-				update: { method: 'POST' },
-				delete: { method: 'DELETE' }
-			});
-		}])
-		.factory('Versions', ['$resource', function($resource){
-			return $resource('/api/versions/:id', {
-				id: '@id'
-			}, {
-				find: { method: 'GET' },
-				update: { method: 'POST' },
-				delete: { method: 'DELETE' }
-			});
-		}])
+			var resource = $resource(
+				'/api/projects/:id',
+				{
+					id: '@id'
+				}, {
+					list: { method: 'GET' },     // /api/projects
+					create: { method: 'POST' },  // /api/projects 
+					get: { method: 'GET' },      // /api/projects/#
+					update: { method: 'POST' },  // /api/projects/# 
+					delete: { method: 'DELETE' } // /api/projects/# 
+				}
+			);
 
-
-		.controller('CheckinController', [function(){
-			var ctrl = this;
-			
-			ctrl.projects = {
-				forms: {
-					add: {
-						visible: false,
-						active: false,
-						promise: null
-					}
+			resource.save = function(project){
+				if (project.id){
+					return this.update(project);
+				} else{
+					return this.create(project);
 				}
 			};
+
+			return resource;
+		}])
+		.factory('Designs', ['$resource', function($resource){
+			var resource = $resource(
+				'/api/designs/:id',
+				{
+					id: '@id'
+				}, {
+					get: { method: 'GET' },      // /api/designs/#
+					update: { method: 'POST' },  // /api/designs/#
+					delete: { method: 'DELETE' } // /api/designs/#
+				}
+			);
+
+			return resource;
+		}])
+		.factory('Versions', ['$resource', function($resource){
+			var resource = $resource(
+				'/api/versions/:id',
+				{
+					id: '@id'
+				}, {
+					get: { method: 'GET' },      // /api/versions/#
+					update: { method: 'POST' },  // /api/versions/#
+					delete: { method: 'DELETE' } // /api/versions/#
+				}
+			);
+
+			return resource;
+		}])
+		.factory('ProjectDesigns', ['$resource', function($resource){
+			var resource = $resource(
+				'/api/projects/:project_id/designs',
+				{
+					project_id: '@project_id',
+					design_id: '@design_id',
+				}, {
+					list: { method: 'GET' },   // /api/projects/#/designs
+					append: { method: 'POST' } // /api/projects/#/designs
+				}
+			);
+
+			return resource;
+		}])
+		.factory('DesignVersions', ['$resource', function($resource){
+			var resource = $resource(
+				'/api/designs/:design_id/versions',
+				{
+					design_id: '@design_id',
+					version_id: '@version_id',
+				}, {
+					list: { method: 'GET' },   // /api/designs/#/versions
+					append: { method: 'POST' } // /api/designs/#/versions
+				}
+			);
+
+			return resource;
+		}])
+		.controller('CheckinController', [function(){
+			var ctrl = this;
 		}]);
 })();
