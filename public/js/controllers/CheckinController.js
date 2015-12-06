2973b6db2a5bc46e3c43481efef8a47679aa9edd
whiterook6 <whiterook6@gmail.com>
2015-12-05 21:03:57 -0800

    Moved storage of projects into projects service. Added wrapper to projects resource. Added forms to projectController.

--- public/js/controllers/CheckinController.js
+++ public/js/controllers/CheckinController.js
@@ -1,7 +1,33 @@
 (function(){
-	angular.module('Check-in').controller('CheckinController', [function(){
+	angular.module('Check-in').controller('CheckinController', ['Projects', function(projects_service){
 		var ctrl = this;
 
-		
+		ctrl.forms = {
+			new_project: {
+				refresh: function(){
+					ctrl.forms.new_project.data = {};
+					ctrl.forms.new_project.promise = null;
+				},
+
+				submit: function(){
+					var promise = ctrl.projects.create(this.data);
+					promise.then(function(){
+						ctrl.forms.new_project.data = {};
+					});
+					ctrl.forms.new_project.promise = promise;
+				},
+
+				is_disabled: function(){
+					return this.promise && !this.promise.$$state.status;
+				},
+
+				can_submit: function(){
+					return this.data && this.data.name;
+				}
+			}
+		};
+		ctrl.forms.new_project.refresh();
+
+		ctrl.projects = projects_service;
 	}]);
 })();
--- public/js/services/ProjectService.js
+++ public/js/services/ProjectService.js
@@ -1,35 +1,71 @@
 (function(){
 	angular.module('Check-in').factory('Projects', ['$resource', function($resource){
-		var resource = $resource(
+		var _resource = $resource(
 			'/api/projects/:id/:model',
 			{
 				id: '@id'
 			}, {
-				index: {          method: 'GET' },    // /api/projects
+				index: {          method: 'GET', isArray: true },    // /api/projects
 				create: {         method: 'POST' },   // /api/projects
 				read: {           method: 'GET' },    // /api/projects/#
 				update: {         method: 'POST' },   // /api/projects/#
 				delete: {         method: 'DELETE' }, // /api/projects/#
 
-				designs:       { method: 'GET',  model: 'designs' }, // /api/projects/#/designs
+				designs:       { method: 'GET',  model: 'designs', isArray: true }, // /api/projects/#/designs
 				create_design: { method: 'POST', model: 'designs' }, // /api/projects/#/designs
 
-				comments:       { method: 'GET',  model: 'comments' }, // /api/projects/#/comments
+				comments:       { method: 'GET',  model: 'comments', isArray: true }, // /api/projects/#/comments
 				create_comment: { method: 'POST', model: 'comments' }, // /api/projects/#/comments
 
-				requirements:       { method: 'GET',  model: 'requirements'}, // /api/projects/#/requirements
-				create_requirement: { method: 'POST', model: 'requirements'}, // /api/projects/#/requirements
+				requirements:       { method: 'GET',  model: 'requirements', isArray: true }, // /api/projects/#/requirements
+				create_requirement: { method: 'POST', model: 'requirements' }, // /api/projects/#/requirements
 			}
 		);
 
-		resource.save = function(project){
-			if (project.id){
-				return this.update(project);
-			} else{
-				return this.create(project);
+		var projects = {
+			_resource: _resource,
+			all: [],
+
+			index: function(){
+				var promise = _resource.index().$promise;
+				promise.then(function(response){
+					projects.all = response;
+				});
+
+				return promise;
+			},
+
+			create: function(project){
+				var promise = _resource.create(project).$promise;
+				promise.then(function(response){
+					projects.all.push(project);
+				});
+
+				return promise;
+			},
+
+			read: function(project){
+				var promise = _resource.read(project).$promise;
+				promise.then(function(response){
+					for (var i = projects.all.length - 1; i >= 0; i--) {
+						var _current = projects.all[i];
+						if (_current.id === project.id){
+							angular.merge(_current, project);
+							break;
+						}
+					}
+				});
+
+				return promise;
+			},
+
+			delete: function(project){
+				var promise = projects._resource.delete(project).$promise;
+
 			}
 		};
 
-		return resource;
+		projects.index();
+		return projects;
 	}]);
 })();
